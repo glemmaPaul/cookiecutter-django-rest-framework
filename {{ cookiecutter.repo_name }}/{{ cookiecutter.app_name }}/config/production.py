eac17ae540c56856043e983da800b285845eb6f3
Paul Oostenrijk <paul@glemma.nl>
2015-05-26 14:54:48 -0400

    feat(wsgi): Added wsgi file
    feat(config/production): Added whitenoise support
    
    feat(manage): Use django-configuration on runtime

--- {{ cookiecutter.repo_name }}/{{ cookiecutter.app_name }}/config/production.py
+++ {{ cookiecutter.repo_name }}/{{ cookiecutter.app_name }}/config/production.py
@@ -116,4 +116,6 @@ class Production(Common):
         CACHES = values.CacheURLValue(default="memcached://127.0.0.1:11211")
     # END CACHING
 
-    # Your production stuff: Below this line define 3rd party library settings
+    # Django Whitenoise
+    STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+    # End Django Whitenoise
